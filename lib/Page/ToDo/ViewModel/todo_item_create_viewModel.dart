dc5e809ec054be788060302e873d8825607482d2
hejinghao <719549112@qq.com>
2021-11-24 18:03:28 +0800

    移除viewmodel

--- lib/Page/ToDo/ViewModel/todo_item_create_viewModel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_life_record/Common/lr_database_tool.dart';
-import 'package:flutter_life_record/Page/ToDo/Models/todo_list_item_model.dart';
-
-class ToDoItemCreateViewModel {
-  Future<int> saveToDoItem(ToDoListItemModel model) async {
-    var id = await LRDataBaseTool.getInstance().insertToDoItem(model);
-    return id;
-  }
-}
